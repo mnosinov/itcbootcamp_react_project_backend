5ff3e1d59a6917e4152e216a335888e41edf84d5
mnosinov <mnosinov@gmail.com>
2023-03-17 19:39:07 +0600

    one veriant of code

--- back.js
+++ back.js
@@ -1,18 +1,39 @@
-const http = require('http'); //node js is in ES5, that is why there is no async await or import. Thise line is equal to import expression in react
-const url = require('url');
 const file = require('fs');
+const express = require('express');
 
-http.createServer( (request, response) => {
-	const path = url.parse(request.url);
-	response.setHeader('Access-Control-Allow-Origin', '*');
-	response.setHeader('Access-Control-Allow-Methods', 'GET,POST');
+const PORT = 8080;
+const HOSTNAME = '192.168.31.137';
+const app = express();
 
-	if (path.pathname === '/person' && request.method === 'GET') {
-		const person = file.readFileSync('./database.json');
-		response.end(JSON.stringify(JSON.parse(person)));
-		return;
-	}
-	response.end( `404: Not Found URL`);
-}).listen(8080, '192.168.31.137', () => {
-	console.log('Server is up!');
+// GET, POST, DELETE, PUT, PATCH, OPTIONS, TRACE, CONNECT, HEAD
+//
+// CRUD - Create, Read, Update, Delete
+// POST - Create
+// GET - Read
+// PUT - Update
+// DELETE - Delete
+
+app.get('/data', (req, res) => {
+	const person = file.readFileSync('./database.json');
+	res.send(JSON.stringify(JSON.parse(person)));
+});
+
+app.post('/data', (req, res) => {
+	const person = file.readFileSync('./database.json');
+	res.send('response to POST method);
+});
+
+app.delete('/data', (req, res) => {
+	const person = file.readFileSync('./database.json');
+	res.send('response to DELETE method')));
+});
+
+app.put('/data', (req, res) => {
+	const person = file.readFileSync('./database.json');
+	res.send('response to PUT method'));
+});
+
+// run server
+app.listen(PORT, HOSTNAME, () => {
+	console.log('Express js server is up!');
 });
